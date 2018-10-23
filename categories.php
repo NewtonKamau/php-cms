7ace9666120746846699f9cc28bc4fa3b3d8e312
Newton <kamaunewton78@gmail.com>
2018-10-23 13:19:30 +0300

    setup of cms system dashboard

---
+++ categories.php
@@ -0,0 +1,80 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Dashboard</title>
+	<link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css" />
+	<script src="js.bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" media="screen" href="css/adminstyles.css" />
+
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-2">
+				<h1>Newton</h1>
+				<ul id="side_menu" class="nav nav-pills nav-stacked">
+					<li><a href="/Dashboard.php"><span class="glyficons glyficon-th"></span> &nbsp; Dashboard</a></li>
+					<li><a href="/#"><span class="glyficons glyficon-list-alt"></span>&nbsp; Add New Post</a></li>
+					<li class="active"><a href="/categories.php"><span class="glyficons glyficon-tags"></span>&nbsp; Categories</a></li>
+					<li><a href="/#"><span class="glyficons glyficon-users"></span>&nbsp; Manage Admins</a></li>
+					<li><a href="/#"><span class="glyficons glyficon-comment"></span>&nbsp; Comments</a></li>
+					<li><a href="/#"><span class="glyficons glyficon-equalizer"></span>&nbsp; Live Blog</a></li>
+					<li><a href="/#"><span class="glyficons glyficon-log-out"></span>&nbsp; LogOut</a></li>
+				</ul>
+                
+			</div> <!-- end of side area of the panel-->
+			<div class="col-sm-10">
+				<h1>Admin Panel</h1>
+				<h4>About</h4>
+				<p><Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<h4>About</h4>
+				<p><Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<h4>About</h4>
+				<p><Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<h4>About</h4>
+				<p><Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<h4>About</h4>
+				<p><Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<h4>About</h4>
+				<p><Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div> <!-- end of main area of the panel-->
+		</div>
+	</div><!-- end of container-->
+ <div id="Footer">
+ 	<hr><p>Theme by | newnyc |&copy:2018-2022 ---All rights reserved.</p>
+ 	
+ </div>
+
+
+</body>
+</html>
